934f42d506b84cb433bb55280bc9801eec270d52
Matthew Pryor <matthewapryor@gmail.com>
2022-05-15 21:45:51 -0400

    initial commit

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// Add near the top of the file
+const apiRoutes = require('./routes/apiRoutes');
+
+router.use(require('./candidateRoutes'));
+
+// Add after Express middleware
+app.use('/api', apiRoutes);
+
+module.exports = router;
